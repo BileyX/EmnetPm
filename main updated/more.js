8676b7f3701412bb255dea05d98b3329080d320d
BileyX <125549520+BileyX@users.noreply.github.com>
2024-09-02 20:41:52 +0300

    Update more.js

--- main updated/more.js
+++ main updated/more.js
@@ -4,7 +4,6 @@
     let btn = document.querySelector(".read-less");
     const y = btn.getBoundingClientRect().top + window.pageYOffset;
     const x = s.getBoundingClientRect().top + window.pageYOffset;  
-  
     function toggleReadMore(){
     if(l.style.display == "inline"){
         for (var i=0; i<p.length; i++) {
